d2120e0d7a2341ab1c4626308811b26b36ee0d57
Virginie <vb@ikimo9.com>
2023-04-05 12:17:37 +0200

    ajout $subscribe + undo/redo implementation

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from "@/stores/CartStore";
+import { ref, reactive } from "vue";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
 
-productStore.fill();
+cartStore.$subscribe((mutation, state) => {
+  console.log(mutation);
+  if (!manualEditingHistory.value) {
+    history.push(JSON.stringify(state));
+    // future.splice(0, future.length);
+  }
+});
 cartStore.$onAction(({ name, args, after, onError }) => {
   if (name === "addItems") {
     console.log("before");
@@ -15,11 +22,41 @@ cartStore.$onAction(({ name, args, after, onError }) => {
     onError((error) => console.log("Action error: ", error.message));
   }
 });
+
+const history = reactive([]);
+const future = reactive([]);
+
+const manualEditingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+
+const undo = () => {
+  if (history.length === 1) return;
+  manualEditingHistory.value = true;
+  future.push(history.pop());
+  cartStore.$state = JSON.parse(history.at(-1));
+  manualEditingHistory.value = false;
+};
+const redo = () => {
+  const latestState = future.pop();
+  if (!latestState) return;
+  manualEditingHistory.value = true;
+  history.push(latestState);
+  cartStore.$state = JSON.parse(latestState);
+  manualEditingHistory.value = false;
+};
+
+productStore.fill();
 </script>
 
 <template>
   <div class="container">
+    {{ history }}
+    {{ future }}
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="redo">Redo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
